390d6c877a27d3d3421ba6faaa5fa79b848a63e6
Steven Venham <deathmock@gmail.com>
2019-09-28 12:25:57 -0400

    Good luck

--- MSVS/include/ClientRequestManager.h
+++ MSVS/include/ClientRequestManager.h
@@ -1,11 +1,55 @@
 #pragma once
 #include "ClientRequest.h"
-
 #include <vector>
+#include "duktape.h"
 
 //Takes a client request and a client, stores it then processes them in a queue returning the result to the client
 class ClientRequestManager {
+public:
+	static ClientRequestManager* instance() {
+		if (!_instance) {
+			_instance = new ClientRequestManager();
+		}
+		return _instance;
+	}
 
+	static bool bindFunctionToString(duk_ret_t(*func)(duk_context *ctx), const int args, const char* name) {
+		return instance()->_bindFunctionToString(func, args, name);
+	}
+	static void processClientRequest(const char* filename, const char* funcname) {
+		instance()->_processClientRequest(filename, funcname);
+	}
 private:
+	ClientRequestManager() {
+		ctx = duk_create_heap_default();
+	}
+	~ClientRequestManager()
+	{
+		duk_destroy_heap(ctx);
+	}
+
+	bool _bindFunctionToString(duk_ret_t(*func)(duk_context *ctx), const int args,const char* name) {
+		duk_push_c_function(ctx, func, args);
+		duk_put_global_string(ctx, name);
+		return true;
+	}
+
+	bool _loadFile(const char* filename) {
+
+	}
+
+	void _processClientRequest(const char* filename, const char* funcname) {
+		/*
+			if (!ctx) { exit(1); }
+			duk_push_c_function(ctx, dummy_upper_case, 1);
+			duk_push_string(ctx, filename);
+			duk_call(ctx, 1);
+			printf("%s -> %s\n", filename, duk_to_string(ctx, -1));
+			duk_pop(ctx);
+		*/
+		duk_eval_string_noresult(ctx, "print('2+3=' + adder(2, 3));");
+	}
+	static ClientRequestManager* _instance;
 	std::vector<ClientRequest> _clientRequests;
+	duk_context *ctx;
 };
--- MSVS/main.cpp
+++ MSVS/main.cpp
@@ -3,13 +3,58 @@
 #include "GameDatabase.h"
 
 #include "SocketServer.h"
+#include "ClientRequestManager.h"
 
 GameDatabase* GameDatabase::_instance;
+ClientRequestManager* ClientRequestManager::_instance;
+
+static int dummy_upper_case(duk_context *ctx) {
+	size_t sz;
+	const char *val = duk_require_lstring(ctx, 0, &sz);
+	size_t i;
+
+	/* We're going to need 'sz' additional entries on the stack. */
+	duk_require_stack(ctx, sz);
+
+	for (i = 0; i < sz; i++) {
+		char ch = val[i];
+		if (ch >= 'a' && ch <= 'z') {
+			ch = ch - 'a' + 'A';
+		}
+		duk_push_lstring(ctx, (const char *)&ch, 1);
+	}
+
+	duk_concat(ctx, sz);
+	return 1;
+}
+
+static duk_ret_t native_print(duk_context *ctx) {
+	printf("%s\n", duk_to_string(ctx, 0));
+	return 0;  /* no return value (= undefined) */
+}
+
+/* Adder: add argument values. */
+static duk_ret_t native_adder(duk_context *ctx) {
+	int i;
+	int n = duk_get_top(ctx);  /* #args */
+	double res = 0.0;
+
+	for (i = 0; i < n; i++) {
+		res += duk_to_number(ctx, i);
+	}
+
+	duk_push_number(ctx, res);
+	return 1;  /* one return value */
+}
 
 int main(int* argc, int ** args) {
 	
 	//Start the server.
 	SocketServer server(80, "127.0.0.1");
+	ClientRequestManager::bindFunctionToString(dummy_upper_case, 1, "upper");
+	ClientRequestManager::bindFunctionToString(native_print, 1, "print");
+	ClientRequestManager::bindFunctionToString(native_adder, -1, "adder");
+	ClientRequestManager::processClientRequest("test", "test2");
 
 	//TODO: Create instance of the socket server.
 	//TODO: load XML documents to take text.
